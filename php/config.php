bf586276dcf68587fd29c029788e5ecbe8b9031c
Boopathiraja Kannan <71979130+kbrkannan@users.noreply.github.com>
2021-07-30 14:45:54 +0530

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$keyId = 'rzp_test_0YaUNwVZcGclLt';
+$keySecret = '12JmZ25taA1UqljVFCwqrEAy';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+//DATABASE CONNECTION DETAILS
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "educore";
---
+++ php/paymentform.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {
+  font-family: Times new roman;
+  font-size: 20px;
+  padding: 8px;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.row {
+  display: -ms-flexbox; 
+  display: flex;
+  -ms-flex-wrap: wrap; 
+  flex-wrap: wrap;
+  margin: 0 -16px;
+}
+
+.col-25 {
+  -ms-flex: 25%; 
+  flex: 25%;
+}
+
+.col-50 {
+  -ms-flex: 50%; 
+  flex: 50%;
+}
+
+.col-75 {
+  -ms-flex: 75%; 
+  flex: 75%;
+}
+
+.col-25,
+.col-50,
+.col-75 {
+  padding: 0 16px;
+}
+
+.container {
+  background-color: #f2f2f2;
+  padding: 5px 20px 15px 20px;
+  border: 1px solid lightgrey;
+  border-radius: 3px;
+}
+
+input[type=text] {
+  width: 100%;
+  margin-bottom: 20px;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+label {
+  margin-bottom: 10px;
+  display: block;
+}
+
+.icon-container {
+  margin-bottom: 20px;
+  padding: 7px 0;
+  font-size: 24px;
+}
+
+.btn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px;
+  margin: 10px 0;
+  border: none;
+  width: 100%;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 17px;
+}
+
+.btn:hover {
+  background-color: #45a049;
+}
+
+a {
+  color: #2196F3;
+}
+
+hr {
+  border: 1px solid lightgrey;
+}
+
+span.price {
+  float: right;
+  color: red;
+}
+
+
+@media (max-width: 800px) {
+  .row {
+    flex-direction: column-reverse;
+  }
+  .col-25 {
+    margin-bottom: 20px;
+  } 
+}
+</style>
+</head>
+<body>
+
+
+<div class="row"  style="padding:100px 300px;">
+  <div class="col-50">
+    <div class="container" >
+      <form  action="pay.php" method="post" style="padding: 25px;">
+      
+        <div class="row" >
+          <div class="col-25">
+            <h3 style="text-align: center;margin:22px 10px;font-family: Times new roman;border-color: rgb(rgb);">E-Learning Platform Enter the details before paying the amount</h3>
+          
+
+            FullName <input type ="text" name ="customername" placeholder="ABC"><br>
+            Email <input type="text" name="email" placeholder="Email"><br>
+            Contact <input type="text" name="contactno" placeholder="9999999999"><br>
+            Address <input type="text" name="address" placeholder="abc street, city, pincode"><br> 
+            PRICE <input type = "text" name="price"><br>
+            
+
+          
+        </div>
+       
+        <input type="submit"  value="Pay Now" class="btn">
+      </form>
+    </div>
+  </div>
+ 
+</div>
+
+</body>
+</html>
